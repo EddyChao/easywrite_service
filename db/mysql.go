e790cf8a6e73a96194c96c111c94bfdadc11cb4a
wilinz <weilizan71@gmail.com>
2025-02-25 16:45:42 +0800

    :truck: refactor docker-compose for networking and naming consistency :pencil: update service configuration and database connection logic :bug: fix MySQL reconnection logic and improve error handling :memo: update README for correct docker service naming

--- db/mysql.go
+++ db/mysql.go
@@ -6,6 +6,8 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"log"
+	"time"
 )
 
 var (
@@ -21,26 +23,58 @@ type MysqlConfig struct {
 }
 
 func (c *MysqlConfig) toUrl() string {
-	return fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", c.Username, c.Password, c.Host, c.Port, c.Database)
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.Username, c.Password, c.Host, c.Port, c.Database)
+}
+
+// 自动重连函数
+func connectWithRetry(mysqlConfig MysqlConfig, maxRetries int, retryInterval time.Duration) *gorm.DB {
+	var db *gorm.DB
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		db, err = gorm.Open(mysql.Open(mysqlConfig.toUrl()), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err == nil {
+			log.Println("MySQL 连接成功！")
+			return db
+		}
+
+		log.Printf("连接 MySQL 失败: %v，%d 秒后重试 (%d/%d)...\n", err, retryInterval/time.Second, i+1, maxRetries)
+		time.Sleep(retryInterval)
+	}
+
+	panic(fmt.Sprintf("无法连接到 MySQL，已重试 %d 次: %v", maxRetries, err))
 }
 
 func InitMySql(mysqlConfig MysqlConfig) {
-	var err error
-	Mysql, err = gorm.Open(mysql.Open(mysqlConfig.toUrl()), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		panic(err)
+	// 允许最多 5 次重试，每次间隔 5 秒
+	Mysql = connectWithRetry(mysqlConfig, 5, 5*time.Second)
+
+	// 自动迁移数据库表结构
+	if err := Mysql.AutoMigrate(&model.User{}, &model.Bill{}, &model.Feedback{}, &model.AppVersion{}); err != nil {
+		panic(fmt.Sprintf("数据库迁移失败: %v", err))
 	}
 
-	err = Mysql.AutoMigrate(&model.User{})
-	if err != nil {
-		panic(err)
-	}
-	err = Mysql.AutoMigrate(&model.Bill{})
-	err = Mysql.AutoMigrate(&model.Feedback{})
-	err = Mysql.AutoMigrate(&model.AppVersion{})
-	if err != nil {
-		panic(err)
+	// 定时检查数据库连接
+	go keepAlive(30*time.Second, mysqlConfig)
+}
+
+// 定时检查数据库连接并重连
+func keepAlive(interval time.Duration, mysqlConfig MysqlConfig) {
+	for {
+		time.Sleep(interval)
+
+		sqlDB, err := Mysql.DB()
+		if err != nil {
+			log.Println("获取数据库连接失败:", err)
+			continue
+		}
+
+		if err = sqlDB.Ping(); err != nil {
+			log.Println("数据库连接丢失，尝试重连...")
+			Mysql = connectWithRetry(mysqlConfig, 5, 5*time.Second)
+		}
 	}
 }
--- docker/README.md
+++ docker/README.md
@@ -1,7 +1,7 @@
 ```shell
- cd .. && docker buildx build --platform linux/amd64 -t easy-write-service -f docker/Dockerfile
+ cd .. && docker buildx build --platform linux/amd64 -t easywrite-service -f docker/Dockerfile .
 ```
 
 ```shell
- cd .. && docker buildx build --platform linux/arm64 -t easy-write-service -f docker/Dockerfile
+ cd .. && docker buildx build --platform linux/arm64 -t easywrite-service -f docker/Dockerfile .
 ```
